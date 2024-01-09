bfcbcd6bb9aa0f1a4337288ec71a1bc5680b68bd
SpangorBen <netflixbenzzinbi@gmail.com>
2024-01-09 11:22:19 +0100

    add helpers folder

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -2,7 +2,21 @@
 
 class Dashboard extends Controller {
 
+    public function __construct(){
+        $this->Category = $this->model('Category');
+        $this->ServiceCategory = $this->service('ServiceCategory');
+    }
+
     public function home(){
+        $this->view('dashboard/home');
+    }
+
+    public function addCategory(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $this->Category->Title = $_POST['title'];
+            $this->Category->Description = $_POST['description'];
+            $this->ServiceCategory->Add($this->Category);
+        }
         $this->view('dashboard/home');
     }
 }
--- app/views/dashboard/home.php
+++ app/views/dashboard/home.php
@@ -151,7 +151,7 @@
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Name</label>
-                        <input type="text" name="category" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Category name" required="">
+                        <input type="text" name="title" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Category name" required="">
                     </div>
                     <!-- <div class="col-span-2 sm:col-span-1">
                         <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
@@ -169,7 +169,7 @@
                     </div> -->
                     <div class="col-span-2">
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category Description</label>
-                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>                    
+                        <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write category description here"></textarea>                    
                     </div>
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
